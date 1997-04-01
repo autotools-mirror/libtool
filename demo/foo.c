87b8075bc962d6fde11fef24bc1fb3e8a4d38e6b
Gordon Matzigkeit <gord@gnu.ai.mit.edu>
1997-04-01 18:29:23 +0000

    Initial revision

---
+++ demo/foo.c
@@ -0,0 +1,29 @@
+/* foo.c -- trivial test function for libfoo
+   Copyright (C) 1996 Free Software Foundation, Inc.
+   This file is part of GNU Libtool.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Written by Gordon Matzigkeit <gord@gnu.ai.mit.edu> */
+#include "foo.h"
+
+static char *package = PACKAGE;
+static char *version = VERSION;
+
+int
+foo ()
+{
+  return FOO_RET;
+}
