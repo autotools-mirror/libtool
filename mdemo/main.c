3c3daa2cad522ce0b3b3ce04fe8bd336b7881229
Thomas Tanner <tanner@ffii.org>
1999-07-01 14:48:44 +0000

    * doc/PLATFORMS: updated * changed my email address

--- mdemo/main.c
+++ mdemo/main.c
@@ -1,6 +1,6 @@
 /* main.c -- mdemo test program
    Copyright (C) 1998-1999 Free Software Foundation, Inc.
-   Originally by Thomas Tanner <tanner@gmx.de>
+   Originally by Thomas Tanner <tanner@ffii.org>
    This file is part of GNU Libtool.
 
 This program is free software; you can redistribute it and/or modify
