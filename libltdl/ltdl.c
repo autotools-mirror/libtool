9bfa9cf6b9554aec8374e19eb143d35efdcf3c32
Peter O'Gorman <peter@pogma.com>
2003-08-01 19:08:14 +0000

    * libtool.m4 (darwin): Use "-undefined dynamic_lookup" if the user set MACOSX_DEPLOYMENT_TARGET to 10.3 or greater. * libltdl/ltdl.c (dyld): Save the error from the first lookup to report in lt_dlerror(). Otherwise the error is always "Symbol _foo not in /usr/lib/libSystem.dylib". Thanks to Chris Zubrzycki for reporting this.

--- libltdl/ltdl.c
+++ libltdl/ltdl.c
@@ -1870,6 +1870,7 @@ sys_dyld_sym (loader_data, module, symbol)
   	NSSymbol *nssym = 0;
   	void *unused;
   	const struct mach_header *mh=NULL;
+  	char saveError[256] = "Symbol not found";
   	if (module == (lt_module)-1)
   	{
   		_dyld_lookup_and_bind(symbol,(unsigned long*)&address,&unused);
@@ -1900,12 +1901,14 @@ sys_dyld_sym (loader_data, module, symbol)
 	}
 	if (!nssym)
 	{
+		strncpy(saveError, lt_int_dyld_error(LT_DLSTRERROR(SYMBOL_NOT_FOUND)), 255);
+		saveError[255] = 0;
 		if (!mh) mh=lt_int_dyld_get_mach_header_from_nsmodule(module);
 		nssym = lt_int_dyld_NSlookupSymbolInLinkedLibs(symbol,mh);
 	}
 	if (!nssym)
 	{
-		LT_DLMUTEX_SETERROR (lt_int_dyld_error(LT_DLSTRERROR(SYMBOL_NOT_FOUND)));
+		LT_DLMUTEX_SETERROR (saveError);
 		return NULL;
 	}
 	return NSAddressOfSymbol(nssym);
