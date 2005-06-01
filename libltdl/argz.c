7e0238b4f3475e6d928104877ce184ab4a18e6d9
Olly Betts <olly@survex.com>
2005-06-01 19:09:00 +0000

    * libltdl/argz.c (argz_create_sep): Fix typo.

--- libltdl/argz.c
+++ libltdl/argz.c
@@ -96,7 +96,7 @@ argz_create_sep (const char *str, int delim, char **pargz, size_t *pargz_len)
   assert (pargz);
   assert (pargz_len);
 
-  /* Make a copy of STR, but replacing each occurence of
+  /* Make a copy of STR, but replacing each occurrence of
      DELIM with '\0'.  */
   argz_len = 1+ strlen (str);
   if (argz_len)
