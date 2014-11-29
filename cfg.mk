6cc831405a3b290735739fbe27807414f37ac4bb
Gary V. Vaughan <gary@gnu.org>
2014-11-29 18:02:15 +0000

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -24,7 +24,7 @@
 update-copyright-env := UPDATE_COPYRIGHT_FORCE=1 UPDATE_COPYRIGHT_USE_INTERVALS=1
 
 # Set format of NEWS
-old_NEWS_hash := e524180c3db06628ad97e3fcb35f6a4b
+old_NEWS_hash := bbe93ee3022140a62e18f71eb31ed365
 
 manual_title = Portable Dynamic Shared Object Management
 
