c4abf359f849187fa7f505a0a34f15f2bd66db24
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2025-09-18 17:16:22 +0300

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -29,7 +29,7 @@ update-release-year:
 		m4/libtool.m4
 
 # Set format of NEWS.
-old_NEWS_hash := d51e62d3a4268be1a7b418b089c528d0
+old_NEWS_hash := 6b55a9c35959c8c4918110ff839bff7d
 
 manual_title = Portable Dynamic Shared Object Management
 
